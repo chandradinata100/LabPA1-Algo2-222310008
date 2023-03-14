07a80bb2d5fddc2764bc10ba6b7358685eea76dc
chandradinata100 <113411375+chandradinata100@users.noreply.github.com>
2023-03-14 07:22:15 +0700

    Add files via upload

---
+++ tugas pertemuan 1/tugas1-02.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+void bubbleSort(string arr[], int n);
+
+int main() {
+    int n;
+    cout << "Jumlah buku yang ingin dimasukkan: ";
+    cin >> n;
+    string judul[n];
+    for (int i = 0; i < n; i++) {
+        cout << "Masukkan judul buku ke-" << i+1 << ": ";
+        cin >> judul[i];
+    }
+    bubbleSort(judul, n);
+    cout << "\nPengurutan judul buku dalam alfabet:\n";
+    for (int i = 0; i < n; i++) {
+        cout << judul[i] << endl;
+    }
+    return 0;
+}
+void bubbleSort(string arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
