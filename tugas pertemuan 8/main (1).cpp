05a0f8c13964208c2480e98684ac8c0da4bbc6c6
chandradinata100 <113411375+chandradinata100@users.noreply.github.com>
2023-05-30 07:52:10 +0700

    Add files via upload

---
+++ tugas pertemuan 8/main (1).cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+  // properties node
+  int data;
+  struct Node *left;
+  struct Node *right;
+
+  // constructor
+  Node(int data) {
+    this->data = data;
+    left = right = NULL;
+  }
+};
+
+// PREORDER TRAVERSAL
+void preorderTraversal(struct Node *node) {
+  if (node == NULL)
+    return;
+
+  cout << node->data << " "; // nampilin data pertama masuk
+  preorderTraversal(node->left);
+  preorderTraversal(node->right);
+}
+
+// postorder traversal
+void postorderTraversal(struct Node *node) {
+  if (node == NULL)
+    return;
+
+  postorderTraversal(node->left);
+  postorderTraversal(node->right);
+  cout << node->data << " ";
+}
+
+// inorder traversal
+void inorderTraversal(struct Node *node) {
+  if (node == NULL)
+    return;
+
+  inorderTraversal(node->left);
+  cout << node->data << " ";
+  inorderTraversal(node->right);
+  cout << node->data << " ";
+}
+
+// fungsi utama
+int main() { 
+  struct Node *root = new Node(1);
+  root->left = new Node(12);
+  root->right = new Node(9);
+  root->left->left = new Node(5);
+  root->left->right = new Node(6);
+
+  cout << "inorder traversal output";
+  inorderTraversal(root);
+  cout << "\npreorder traversal output";
+  preorderTraversal(root);
+  cout << "\npostorder traversal output";
+  postorderTraversal(root);
+}
